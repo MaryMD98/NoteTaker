a059cfd7b5657ef6bf4cb9fbcc2692100fbf3d70
Maribel Montes <maribel.doris@gmail.com>
2022-02-03 16:27:30 -0600

    added app get to the server.js

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
-const express = require ('express');
+const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const PORT = 3001;
 
-const app = express ();
+const app = express();
+
+// custom middlware to display the info in a cyan color
+const middleware = (req, res, next) => {
+    const cyan = '\x1b[36m';
+    console.info(`📘 ${cyan}${req.method} request to ${req.path}`);
+    next();
+};
+
+// inittialize the routes
+app.use(middleware);
+
+//middleware for parsing JSON and urlencoded ofr data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET  Route for homepage
+app.get('/', (req,res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for pages
+app.get('/page', (req,res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () => 
+    console.log(`APP listening to requests at http://localhost:${PORT} 🏎️`)
+);
